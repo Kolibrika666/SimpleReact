48c96c36f5f040d035f3067c01a972d73bae9235
Kolibrika666 <kolibrika666@gmail.com>
2025-04-12 20:31:48 +0700

    init: ListItems

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -2,10 +2,11 @@ import Container from 'react-bootstrap/Container';
 import ListItem from './ListItem';
 
 function List() {
+  const listNums = ['1', '2', '3', '4', '5', '6', '7']
   return (
     <Container>
       <h5 className="text-center">Список</h5>
-      <ListItem num="1"/>
+      {listNums.map(el=><ListItem num={el}/>)}
     </Container>
   );
 }
