687d3de76c33a1adec77bd836561f88f66917308
Kolibrika666 <kolibrika666@gmail.com>
2025-04-12 20:27:18 +0700

    init: props

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -1,17 +1,11 @@
 import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
-import { Button } from 'bootstrap';
+import ListItem from './ListItem';
 
 function List() {
   return (
-<Container>
+    <Container>
       <h5 className="text-center">Список</h5>
-      <Row className="justify-content-md-center">
-        <Col md="auto">1</Col>
-        <Col md="auto">элемент</Col>
-        <Col md="auto">списка</Col>
-      </Row>
+      <ListItem num="1"/>
     </Container>
   );
 }
